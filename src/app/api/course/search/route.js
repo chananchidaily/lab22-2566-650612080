0293d980b9e6e2698082dc1bd6b86b7f19f20fe7
Tonkhao <chananchida_tha@cmu.ac.th>
2023-09-28 16:31:04 +0700

    q

--- src/app/api/course/search/route.js
+++ src/app/api/course/search/route.js
@@ -5,8 +5,17 @@ export const GET = async (request) => {
   const searchText = request.nextUrl.searchParams.get("searchText");
   const prisma = getPrisma();
 
-  //Modify following line so that it find course with "searchText" variable
-  const courses = await prisma.course.findMany({});
+  const courses = await prisma.course.findMany({
+    where: {
+      title: {
+        contains: searchText,
+        mode: "insensitive",
+      },
+    },
+    orderBy: {
+      courseNo: "asc",
+    },
+  });
 
   return NextResponse.json({ ok: true, courses });
 };
